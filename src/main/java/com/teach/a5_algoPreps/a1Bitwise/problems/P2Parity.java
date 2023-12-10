deef2d50cdcb5f6400563d97ccf33d4472b5298a
Nitin K Chaurasia <nitin.chaurasia@davita.com>
2023-12-10 00:03:49 -0700

    github push

--- src/main/java/com/teach/a5_algoPreps/a1Bitwise/problems/P2Parity.java
+++ src/main/java/com/teach/a5_algoPreps/a1Bitwise/problems/P2Parity.java
@@ -4,7 +4,36 @@ public class P2Parity {
     public static void main(String[] args) {
         int x = -237;
         System.out.println(Integer.toBinaryString(x));
+        method1(x);
+        method2(x);
+    }
 
-       // x & (x-1);
+    private static void method2(int x) {
+        boolean parity = false;//false for even cases, else true
+        while(x != 0){
+            x = x & (x - 1);//Unset the Rightmost set bit
+            parity = !parity;
+        }
+
+        if(!parity){
+            System.out.println("even parity");
+        } else {
+            System.out.println("odd parity");
+        }
+    }
+
+    private static void method1(int x) {
+        int count = 0;
+        while(x != 0){
+            x = x & (x - 1);//Unset the Rightmost set bit
+            count++;
+        }
+        System.out.println("total ones = " + count);
+
+        if(count%2 == 0){
+            System.out.println("even parity");
+        } else {
+            System.out.println("odd parity");
+        }
     }
 }
